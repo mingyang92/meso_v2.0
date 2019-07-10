bbd8340e4d02a77f6e831f2e9fc4f3500b4598ef
lyy901207 <lyy901207@gmail.com>
2019-07-10 12:36:51 +0900

    Update readOd.py
    update gen dist

--- util/readOd.py
+++ util/readOd.py
@@ -57,8 +57,7 @@ def genDriverValueTimeGen(medianValueTime):
     :param medianValueTime:
     :return:
     """
-    #sigma = medianValueTime / 5.0
-    sigma = medianValueTime * 5.0
+    sigma = medianValueTime / 5.0
     return np.random.normal(medianValueTime, sigma, 1)[0]
 
 def genProbLaneChange(type):
@@ -275,7 +274,7 @@ def genVehicle(tsPairNodePairTypeMap, distribution, vehicleId, medianValueTime, 
         endTs = listTsPair[-1]
         duration = endTs - startTs
         mu = 900 ## need to be set
-        sigma = 100 ## need to be set
+        sigma = 1000 ## need to be set
 
         def normalTs(mu, sigma, size):
             """
